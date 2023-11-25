9e8c94f8cdb66ccf7c70703aa9095a610d8fab28
PUNGZI <ashyoo1367@gmail.com>
2023-11-25 15:26:45 +0900

    test(CarsTest): 문자열로 들어온 자동차가 리스트로 정상 저장되는지 테스트

---
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -0,0 +1,18 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Arrays;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class CarsTest {
+    @DisplayName("문자열로 들어온 자동차 이름들이 ,를 기준으로 리스트 형태로 저장되는가")
+    @ValueSource(strings = {"qwer,asdf,zxcv,tyu", "ter,gdf,v,wwe"})
+    @ParameterizedTest
+    void saveCarsStringToCarsList(String carsCsv) {
+        Cars cars = new Cars(carsCsv);
+        assertThat(cars.getCars()).isEqualTo(Arrays.asList(carsCsv.split(",")));
+    }
+}
