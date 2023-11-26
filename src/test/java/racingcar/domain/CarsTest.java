8a7dac4099ca806c27b72c98ecbd6a6af9e42955
PUNGZI <ashyoo1367@gmail.com>
2023-11-26 20:16:18 +0900

    test(CarsTest): 테스트 케이스 추가

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 public class CarsTest {
     @DisplayName("문자열로 들어온 자동차 이름들이 ,를 기준으로 리스트 형태로 저장되는가")
-    @ValueSource(strings = {"qwer,asdf,zxcv,tyu", "ter,gdf,v,wwe"})
+    @ValueSource(strings = {"qwer,asdf,zxcv,tyu", "ter,gdf,v,wwe", "qerdd"})
     @ParameterizedTest
     void saveCarsStringToCarsList(String carsCsv) {
         Cars cars = new Cars(carsCsv);
