1b199a2c6d04499db23303798fbdec43f202d51a
PUNGZI <ashyoo1367@gmail.com>
2023-11-28 02:49:05 +0900

    feat(RaceController): 도메인 통합

--- src/main/java/racingcar/controller/RaceController.java
+++ src/main/java/racingcar/controller/RaceController.java
@@ -1,35 +1,45 @@
 package racingcar.controller;
 
+import camp.nextstep.edu.missionutils.Console;
 import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
+import racingcar.domain.Cars;
 import racingcar.domain.Race;
+import racingcar.domain.RaceCount;
 import racingcar.view.OutputView;
 
 public class RaceController {
-    private Race race;
+    private static final int INIT_STATUS_NUMBER = 0;
+
+    private Cars cars;
+    private RaceCount raceCount;
 
     public void start() {
-        // inputData
-        // runRace();
+        String a = Console.readLine();
+        String b = Console.readLine();
+        cars = new Cars(a);
+        raceCount = new RaceCount(b);
+        runRace(cars.getCars(), raceCount.getRaceCount());
     }
 
     private void runRace(List<String> cars, int raceCount) {
-        HashMap<String, Integer> carStatus = initCarStatus(cars);
-        runRaceIteration(carStatus, raceCount);
+        OutputView.printCarsStatusMessage();
+        runRaceIteration(initCarStatus(cars), raceCount);
     }
 
     private void runRaceIteration(HashMap<String, Integer> carsStatus, int raceCount) {
         HashMap<String, Integer> newCarsStatus = carsStatus;
         for (int raceIndex = 0; raceIndex < raceCount; raceIndex++) {
-            race = new Race(newCarsStatus);
+            Race race = new Race(newCarsStatus);
             newCarsStatus = race.getCarStatusLogs();
-            OutputView.printCarStatus(newCarsStatus);
+            OutputView.printCarsStatus(newCarsStatus);
         }
+        Race race = new Race(newCarsStatus);
     }
 
     private HashMap<String, Integer> initCarStatus(List<String> cars) {
         return (HashMap<String, Integer>) cars.stream()
-                .collect(Collectors.toMap(key -> key, value -> 0));
+                .collect(Collectors.toMap(key -> key, value -> INIT_STATUS_NUMBER));
     }
 }
