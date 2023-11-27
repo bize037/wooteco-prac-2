3732077e7fbcbfe1b20ac5b5a178bdc7bfb80b6e
PUNGZI <ashyoo1367@gmail.com>
2023-11-28 02:30:02 +0900

    fix(OutputView): "실행 결과"가 반복돼서 출력되는 오류 수정

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,11 +3,14 @@ package racingcar.view;
 import java.util.HashMap;
 
 public class OutputView {
-    public static void printCarStatus(HashMap<String, Integer> carsStatus) {
+
+    public static void printCarsStatusMessage() {
         System.out.println("실행 결과");
+    }
+    public static void printCarsStatus(HashMap<String, Integer> carsStatus) {
         carsStatus.forEach((car, carStatus) -> {
             System.out.println(car + " : " + "-".repeat(carStatus));
         });
-
+        System.out.println();
     }
 }
