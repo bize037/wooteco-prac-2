52c2da603e58e7a58b72469a7312075269cb7b05
PUNGZI <ashyoo1367@gmail.com>
2023-11-28 22:04:10 +0900

    refactor(CarsTest): 문자열 상수화

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.common.constants.Unit;
 
 public class CarsTest {
     @DisplayName("문자열로 들어온 자동차 이름들이 ,를 기준으로 리스트 형태로 저장되는가")
@@ -13,6 +14,6 @@ public class CarsTest {
     @ParameterizedTest
     void saveCarsStringToCarsList(String carsCsv) {
         Cars cars = new Cars(carsCsv);
-        assertThat(cars.getCars()).isEqualTo(Arrays.asList(carsCsv.split(",")));
+        assertThat(cars.getCars()).isEqualTo(Arrays.asList(carsCsv.split(Unit.COMMA.getUnit())));
     }
 }
